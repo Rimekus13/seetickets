78095e8549fb515897fcccd8dfe8fd52db781857
ALX <alexmonac13@gmail.com>
2023-06-08 12:34:59 +0200

    Fetch1 : Récupération des produits à vendre

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-d;
+const fetch1 = document.querySelector("#fetch1");
+
+fetch1.addEventListener("click", () => {
+  const url =
+    "https://front.apirecette.digitick-ppe.com/v1.1/distribution/salesChannels/13357/calendar/day/2023-06-08?lang=fr";
+
+  const headers = {
+    Authorization: "Bearer 1aa8b6e14ba3760a7ab23cce3aebdc75418843a2",
+  };
+  fetch(url, {
+    headers: headers,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
