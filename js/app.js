98a2c9a5e4b46576b0f4397918e97be1997073b3
OMA <alexmg79@hotmail.fr>
2023-06-09 14:58:18 +0200

    Avancment css

--- js/app.js
+++ js/app.js
@@ -16,3 +16,25 @@ button.addEventListener("click", function() {
     console.log("ok!")
     main.appendChild(info)
 });
+
+    // Fonction pour envoyer l'e-mail de confirmation
+    function envoyerEmail() {
+      Email.send({
+        Host: "john.doe@seetickets.fr",
+        Username: "username@yourisp.com",
+        Password: "password",
+        To: "client@example.com",
+        From: "doe@seetickets.fr",
+        Subject: "Confirmation de commande",
+        Body: "Votre commande a été passée avec succès !",
+      }).then(function (message) {
+        alert("E-mail de confirmation envoyé !");
+      });
+    }
+
+    // Appel de la fonction pour envoyer l'e-mail au chargement de la page
+    window.onload = function () {
+      envoyerEmail();
+    };
+  
+
